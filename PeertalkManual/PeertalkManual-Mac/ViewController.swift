e3702862d65fce789497c109bbd9e7fe9966045d
kirankunigiri <kirankunigiri@gmail.com>
2017-01-07 21:42:31 -0800

    Created Xcode project

---
+++ PeertalkManual/PeertalkManual-Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PeertalkManual-Mac
+//
+//  Created by Kiran Kunigiri on 1/7/17.
+//  Copyright © 2017 Kiran. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ PeertalkManual/PeertalkManual-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeertalkManual-iOS
+//
+//  Created by Kiran Kunigiri on 1/7/17.
+//  Copyright © 2017 Kiran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
