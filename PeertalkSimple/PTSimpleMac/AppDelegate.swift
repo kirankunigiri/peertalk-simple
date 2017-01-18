8c3a13a23a2b1b855d35b72070b7cbd71e5e24ea
kirankunigiri <kirankunigiri@gmail.com>
2017-01-17 23:41:49 -0800

    Refactored project and code cleanup

---
+++ PeertalkSimple/PTSimpleMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PTSimpleMac
+//
+//  Created by Kiran Kunigiri on 1/17/17.
+//  Copyright © 2017 Kiran. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PeertalkSimple/PeertalkSimple-Bridging-Header.h
@@ -0,0 +1,20 @@
+//
+//  Header.h
+//  Peertalk iOS
+//
+//  Created by Kiran Kunigiri on 12/27/16.
+//  Copyright © 2016 Kiran. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import "Peertalk.h"
+#import "PTChannel.h"
+#import "PTPrivate.h"
+#import "PTProtocol.h"
+#import "PTUSBHub.h"
+#import "PTProtocol.h"
+
+
+#endif /* Header_h */
